04278f9674c28e03f2a4cd40307526efe46ab6d9
Etnik Jakupi <jakupietnik1@gmail.com>
2021-12-14 10:34:56 +0100

    rregullimi i dukjes se log in dhe sign up

---
+++ js/login.js
@@ -0,0 +1,11 @@
+function Metoda1(){
+    var emri=document.querySelector("#em1");
+    
+    //if(emri.value.length() =< 11 || emri.value=="" )
+    //console.log(" inputi emrit gabim");
+
+    var email=document.querySelector("#em1");
+
+    if(! email.contains("@")) console.log(" nuk kemi shtypur emaail");
+
+}
