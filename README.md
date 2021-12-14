81995842fe83651b3db5487b2f58ea030bb98128
ArberZe <az51866@ubt-uni.net>
2021-12-14 17:29:25 +0100

    go to readme.md file for more details

--- README.md
+++ README.md
@@ -4,3 +4,10 @@
 #
 -krijimi dhe stilizimi te faqes categories.html
 -shtimi i tagjeve <link> per ndryshimin e Font-it ne fajllat kryesor: index.html dhe ndryshimi fontit ne fajllin style.css  
+
+//Arber
+#
+-krijimi fajllit categories.css
+-krijimi i fajllit navbar.html dhe stilizimi navbarit
+-krijimi i fajllit kryesor index.php ku do te hyjne te gjithe fajllat tjere, shtimi i fajllave navbar.html dhe categories.html
+
---
+++ htmlp/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+        <title>ecomerce</title>
+		<link rel="stylesheet" href="../style/style.css">
+
+        <!--Linqet per FONT-STYLE-->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <?php include "navbar.html" ?>
+        
+        <?php include "categories.html" ?>
+        
+		<script src="../js/main.js"></script>
+    </body>
+</html>
